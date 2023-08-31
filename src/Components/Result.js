74350aa3a1b6955d8c10deeef039b59d79bd1309
Akinola Adewole <adewoleakinola24@gmail.com>
2023-08-31 17:28:32 +0100

    Quiz App

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -7,17 +7,32 @@ const Result = () => {
 
  let username=userDetails.username
  let email= userDetails.email
- console.log(email)
+//  console.log(email)
 
 
   const state1= useSelector(state=>state.correctScore)
   const wrong=useSelector(state=>state.wrongScore)
+
+  let message = "";
+  const totalScore = state1.score;
+
+  if (totalScore > 15) {
+    message = "Congratulations! You did an excellent job!";
+  } else if (totalScore >= 10) {
+    message = "Great job! You scored well.";
+  } else if (totalScore >= 5) {
+    message = "You passed the quiz!";
+  } else {
+    message = "You can improve. Keep trying!";
+  }
+
   return (
     <>
 
     <div>{username}</div>
     <div>You have completed the Quiz</div>
     <div>Total Score: {state1.score}/{quizz.length} </div>
+    <div>{message}</div>
         
     </>
   )
